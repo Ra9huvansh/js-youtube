308523423835cc687ed61df42dfce2e63a36282c
Raghuvansh <166227043+Ra9huvansh@users.noreply.github.com>
2024-07-17 20:11:10 +0000

    conversions done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,9 +16,9 @@ let isLoggedIn = undefined
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(typeof booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -28,7 +28,44 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
+// console.log(stringNumber)
 
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+// *********************************** Operations ******************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+//console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(2 + 2 + "1")
+
+// console.log(+true) // => 1
+// console.log(+"") // => 0
+
+let num1, num2, num3
+
+num1=num2=num3=2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+
 
