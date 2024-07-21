c977a7096ec55ea67b447e3bbfc0f8a700f0ea3b
Raghuvansh <166227043+Ra9huvansh@users.noreply.github.com>
2024-07-21 19:55:06 +0000

    json talk

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -36,9 +36,36 @@ const users = [
     }
 ]
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLogged'))
+// console.log(tinderUser.hasOwnProperty('isLogged'))
+
+// destructuring in objects
+
+const course = { 
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor)
+console.log(instructor)
+
+// {
+//     "name": "raghu",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
 
